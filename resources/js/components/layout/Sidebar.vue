a5780a5e48e6768651694e98b44a750aba1426d1
Ambi <anghelo11022021@gmail.com>
2024-10-10 13:32:07 +0800

    uncomment the usermanagement also removed the  <BreadCrumbs /> in the RICTU index

--- resources/js/components/layout/Sidebar.vue
+++ resources/js/components/layout/Sidebar.vue
@@ -301,13 +301,13 @@ export default {
                         },
                     ]
                 },
-                // {
-                //     link: '/settings/user-management/',
-                //     name: 'Settings',
-                //     tooltip: 'Setings',
-                //     icon: 'gear',
-                //     class: 'menu-icon-custom',
-                // }
+                {
+                    link: '/settings/user-management/',
+                    name: 'User Management',
+                    tooltip: 'User Management',
+                    icon: 'gear',
+                    class: 'menu-icon-custom',
+                }
 
             ],
         },
